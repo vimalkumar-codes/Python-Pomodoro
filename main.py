6c6b358a66daa3f03a13597c580b0bc287c0eacc
Vimal Kumar <vimalkumar.codes@gmail.com>
2026-05-07 15:51:42 +0530

    Complete the Application's User Interface (UI

--- main.py
+++ main.py
@@ -21,11 +21,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(row=2, column=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(row=3, column=1)
 
 
 window.mainloop()
